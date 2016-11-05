c620c6b2b156c47a59bf2e9d1b41c63d74955453
Jared <jaredlgw@outlook.com>
2016-11-05 15:40:20 +1100

    Added files

---
+++ src/jayson/JSONtest1.java
@@ -0,0 +1,5 @@
+package jayson;
+
+public class JSONtest1 {
+	
+}
---
+++ src/jayson/JsonEncodeDemo2.java
@@ -0,0 +1,30 @@
+package jayson;
+
+import java.io.*;
+
+import org.json.simple.JSONObject;
+
+class JsonEncodeDemo2 {
+
+   @SuppressWarnings("unchecked")
+   public static void main(String[] args){
+	
+      JSONObject obj = new JSONObject();
+
+      obj.put("name","foo");
+      obj.put("num",new Integer(100));
+      obj.put("balance",new Double(1000.21));
+      obj.put("is_vip",new Boolean(true));
+
+      StringWriter out = new StringWriter();
+      try {
+		obj.writeJSONString(out);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+      
+      String jsonText = out.toString();
+      System.out.print(jsonText);
+   }
+}
